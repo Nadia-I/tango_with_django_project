37bfa6ced3a03018c3a2c921ff6eec068eb5965f
“Nadia <2763374I@student.gla.ac.uk>
2026-03-02 12:48:00 +0000

    ch 5 excercises up to deleting and recreating db

--- populate_rango.py
+++ populate_rango.py
@@ -13,6 +13,8 @@ def populate():
     # This might seem a little bit confusing, but it allows us to iterate
     # through each data structure, and add the data to our models.
 
+    
+
     python_pages = [
         {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
@@ -30,10 +32,15 @@ def populate():
         {'title':'Flask','url':'http://flask.pocoo.org'} 
     ]
 
+    #as in categoies
     cats = {'Python': {'pages': python_pages},
             'Django': {'pages': django_pages},
             'Other Frameworks': {'pages': other_pages} 
             }
+    
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
     
     
     # If you want to add more categories or pages,
@@ -43,29 +50,33 @@ def populate():
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
-    
+
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
     
-    def add_page(cat, title, url, views=0):
-        p = Page.objects.get_or_create(category=cat, title=title)[0]
-        p.url=url
-        p.views=views
-        p.save()
-        return p
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
 
-    def add_cat(name):
-        c = Category.objects.get_or_create(name=name)[0]
-        c.save()
-        return c
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
     
     # Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
+
+
