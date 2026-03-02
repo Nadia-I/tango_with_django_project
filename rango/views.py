d88ab835ef884b792f0a4d356e1cbcd26e65a2ea
“Nadia <2763374I@student.gla.ac.uk>
2026-03-02 19:19:18 +0000

    chapter 9 excercises after passing tests

--- rango/views.py
+++ rango/views.py
@@ -51,30 +51,23 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context=context_dict)
 
-
+@login_required #also removed comments cause no longer needed
 def add_category(request):
     form = CategoryForm()
-    # A HTTP POST?
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-        # Have we been provided with a valid form?
 
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
             return redirect('/rango/')
         
         else:
-            # The supplied form contained errors - just print them to the terminal.
             print(form.errors)
     
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -163,7 +156,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
